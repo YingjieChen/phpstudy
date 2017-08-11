1a6f4f2eb2c2dfdc7c91eec8a61f829bcaaaedda
yingjiechen <YingjieChen@live.cn>
2017-08-11 10:31:14 +0800

    Set a spacial one to be the sender

--- mail.php
+++ mail.php
@@ -1,3 +1,13 @@
 <?php
-	mail("1038546502@qq.com","This is a subject","This is a message");
+	/*$headers[]	=	'From: yingjiechen@live.cn';
+	$headers[]      =       'Reply-To: 1038546502@qq.com';
+	$headers[]      =       'X-Mailer: PHP/'.phpversion();*/
+
+	/*$headers = 'From: yingjiechen@live.cn' . "\r\n" .
+	'Reply-To: 1038546502@qq.com' . "\r\n" .
+	'X-Mailer: PHP/' . phpversion();*/
+	$headers = 'From: support@HAHAHAHA.com' . "\r\n" .
+	'Reply-To: 1038546502@qq.com' . "\r\n" .
+	'X-Mailer: PHP/' . phpversion();
+	mail("736291739@qq.com","This is a subject","This is a message",$headers);
 ?>
