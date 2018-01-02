02a7394b296427e84c7f0ee817c1e7d02097557e
yingjiechen <YingjieChen@live.cn>
2018-01-02 14:36:07 +0800

    fix the replacing code.

--- replace.php
+++ replace.php
@@ -1,5 +1,6 @@
 <?php
-	$sourcestr	=	"Mens Spike Earrings Huggie Hoop,For Guy,Gothic Punk Rocker,Gift,Goth&Earrings,Gothic,Statement,Minimalist,Black,Stainless Steel";
-	$desstr		=	preg_replace('/,| |\&/i',"-",$sourcestr);
+	$sourcestr	=	'Mens Spike Earrings Huggie Hoop,For Guy,Gothic Punk Rocker,Gift,Goth&Earrings,Gothic,Statement,Minimalist,Black,Stainless Steel';
+	$desstr         =       strtolower($sourcestr);
+	$desstr		=	preg_replace('/[^a-zA-Z0-9](.+?)/i',"-",$desstr);
 	echo $desstr;
 ?>
