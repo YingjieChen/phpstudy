1e7e9242399134b708718a5436c746f10b5027ed
yingjiechen <YingjieChen@live.cn>
2018-02-15 23:47:09 +0800

    complete code

---
+++ nn.php
@@ -0,0 +1,10 @@
+<?php
+	$v1	=	2;
+	$v2	=	2;
+	$v3	=	0;
+	$v4	=	0;
+	$vv	=	2;
+	$four	=	$v1|$v2|$v3|$v4;
+	$middle = 	$vv-1;
+	echo $four;
+?>
--- nokia_buildind_rule.php
+++ nokia_buildind_rule.php
@@ -20,31 +20,37 @@
         }
 	
 	function ispass($numberset){
-		for($x=0;$x<4;$x++){
-			for($y=0;$y<4;$y++){
-				$v1	=	isset($numberset[$x+1][$y])?isset($numberset[$x+1][$y]):0;
-				$v2	=	isset($numberset[$x][$y+1])?isset($numberset[$x][$y+1]):0;
-				$v3	=	isset($numberset[$x-1][$y])?isset($numberset[$x-1][$y]):0;
-				$v4	=	isset($numberset[$x][$y-1])?isset($numberset[$x][$y-1]):0;
+		$result	=	true;
+		$fournumber	=	0;
+		for($x=0;$x<5;$x++){
+			for($y=0;$y<5;$y++){
+				$v1	=	isset($numberset[$x+1][$y])?$numberset[$x+1][$y]:0;
+				$v2	=	isset($numberset[$x][$y+1])?$numberset[$x][$y+1]:0;
+				$v3	=	isset($numberset[$x-1][$y])?$numberset[$x-1][$y]:0;
+				$v4	=	isset($numberset[$x][$y-1])?$numberset[$x][$y-1]:0;
 				$four	=	$v1|$v2|$v3|$v4;
 				$middle	=	$numberset[$x][$y]-1;
-				if($middle&$four==$middle){
+				if(($middle&$four)==$middle||$numberset[$x][$y]==1){
+					if($numberset[$x][$y]==8){ $fournumber++; }
 					continute;
 				}else{
-					return false;
+					$result =  false;
 				}
 			}
 		}
-		return true;
+		if($fournumber<3){
+			$result = false;
+		}
+		return $result;
 	}
-	
+
 	for($a=0;$a<4;$a++){
 			$numberset[0][0]      =       $number_pixel[$a];
 			for($b=0;$b<4;$b++){
 				$numberset[0][1]      =       $number_pixel[$b];
 				for($c=0;$c<4;$c++){
 					$numberset[0][2]      =       $number_pixel[$c];
-                    for($d=0;$d<4;$d++){
+					for($d=0;$d<4;$d++){
 						$numberset[0][3]      =       $number_pixel[$d];
 						for($e=0;$e<4;$e++){
 							$numberset[0][4]      =       $number_pixel[$e];
@@ -69,37 +75,37 @@
 																for($o=0;$o<4;$o++){
 																	$numberset[2][4]      =       $number_pixel[$o];
 																	for($p=0;$p<4;$p++){
-																		$numberset[3][0]      =       $number_pixel[$p];
-																		for($q=0;$q<4;$q++){
-																			$numberset[3][1]      =       $number_pixel[$q];
-																			for($r=0;$r<4;$r++){
-																				$numberset[3][2]      =       $number_pixel[$r];
-																				for($s=0;$s<4;$s++){
-																					$numberset[3][3]      =       $number_pixel[$s];
-																					for($t=0;$t<4;$t++){
-																						$numberset[3][4]      =       $number_pixel[$t];
-																						for($u=0;$u<4;$u++){
-																							$numberset[4][0]      =       $number_pixel[$u];
-																							for($v=0;$v<4;$v++){
-																								$numberset[4][1]      =       $number_pixel[$v];
-																								for($w=0;$w<4;$w++){
-																									$numberset[4][2]      =       $number_pixel[$w];
-																									for($x=0;$x<4;$x++){
-																										$numberset[4][3]      =       $number_pixel[$x];
-																										for($y=0;$y<4;$y++){
-																											$numberset[4][4]      =       $number_pixel[$y];
-				if(ispass($numberset)){
-					output_result($numberset);
-				}
-																										}
-																									}
-																								}
-																							}
-																						}
-																					}
-																				}
-																			}
-																		}
+							$numberset[3][0]      =       $number_pixel[$p];
+							for($q=0;$q<4;$q++){
+								$numberset[3][1]      =       $number_pixel[$q];
+								for($r=0;$r<4;$r++){
+									$numberset[3][2]      =       $number_pixel[$r];
+									for($s=0;$s<4;$s++){
+										$numberset[3][3]      =       $number_pixel[$s];
+										for($t=0;$t<4;$t++){
+											$numberset[3][4]      =       $number_pixel[$t];
+											for($u=0;$u<4;$u++){
+												$numberset[4][0]      =       $number_pixel[$u];
+												for($v=0;$v<4;$v++){					
+													$numberset[4][1]      =       $number_pixel[$v];
+													for($w=0;$w<4;$w++){
+														$numberset[4][2]      =       $number_pixel[$w];
+														for($x=0;$x<4;$x++){
+															$numberset[4][3]      =       $number_pixel[$x];
+															for($y=0;$y<4;$y++){
+																$numberset[4][4]      =       $number_pixel[$y];
+																if(ispass($numberset)){
+																	output_result($numberset);
+																}
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
 																	}
 																}
 															}
